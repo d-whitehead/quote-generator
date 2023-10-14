dba0dd1bdfb42bcbe0a04d7c5b41dcb11dfb722b
Damien <damien.whitehead@gmail.com>
2023-10-14 10:24:18 -0400

    quote generator final

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+
 let apiQuotes = [];
 
 // Quotes from API
 function newQuote() {
     // Pick Random Quote from API Quotes
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // Replace unknown author with an unknown
+    if (!quote.author) {
+       authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Quote length for styling
+    if (quote.text.length > 50) {
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+    quoteText.textContent = quote.text;
 }
 async function getQuotes() {
     const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
@@ -17,5 +37,14 @@ async function getQuotes() {
     }
 }
 
+// Tweet a Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textConent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 // On Load
 getQuotes();
